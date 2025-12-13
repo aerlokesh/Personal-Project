58d3528d55e2852ef09963cfcdd8b43b00c6e8ae
Lokesh Singh <aerloki@amazon.com>
2025-12-13 22:49:20 +0100

    Add Kafka consumer configuration for order notification and inventory consumers
    Added consumer factory and listener container factory:
    - ConsumerFactory with Jackson JSR310 support
    - JsonDeserializer configured for LocalDateTime
    - ConcurrentKafkaListenerContainerFactory for @KafkaListener
    
    This enables:
    - OrderNotificationConsumer to send order confirmation emails
    - InventoryUpdateConsumer to process inventory updates
    - Both consumers deserialize OrderPlacedEvent with date/time fields

--- src/main/java/com/aerloki/personal/project/Personal/Project/config/KafkaConfig.java
+++ src/main/java/com/aerloki/personal/project/Personal/Project/config/KafkaConfig.java
@@ -1,15 +1,21 @@
 package com.aerloki.personal.project.Personal.Project.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import com.aerloki.personal.project.Personal.Project.event.OrderPlacedEvent;
@@ -77,5 +83,36 @@ public class KafkaConfig {
     @Bean
     public KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
+    }
+    
+    @Bean
+    public ConsumerFactory<String, OrderPlacedEvent> consumerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "ecommerce-group");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        
+        // Configure ObjectMapper to handle Java 8 date/time types
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        
+        JsonDeserializer<OrderPlacedEvent> jsonDeserializer = new JsonDeserializer<>(OrderPlacedEvent.class, objectMapper);
+        jsonDeserializer.addTrustedPackages("*");
+        
+        return new DefaultKafkaConsumerFactory<>(
+            configProps,
+            new StringDeserializer(),
+            jsonDeserializer
+        );
+    }
+    
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, OrderPlacedEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, OrderPlacedEvent> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
     }
 }
